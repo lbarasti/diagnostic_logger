31aa01d8760a1e522e208b817088c75bb4567563
lbarasti <lbarasti@users.noreply.github.com>
2020-10-08 21:08:41 +0100

    Upgrade to Crystal 0.35.1

--- src/lib/config.cr
+++ src/lib/config.cr
@@ -2,6 +2,41 @@ require "yaml"
 require "logger"
 require "./appenders"
 
+module YamlToTimeSpan
+  def self.from_yaml(ctx : YAML::ParseContext, node : YAML::Nodes::Node) : Time::Span
+    unless node.is_a?(YAML::Nodes::Scalar)
+      node.raise "Expected scalar, not #{node.class}"
+    end
+
+    (node.value.to_f? || node.value.to_i).seconds
+  end
+end
+
+record BatchConfig, size : Int32, interval : Time::Span do
+  include YAML::Serializable
+
+  @[YAML::Field(converter: YamlToTimeSpan)]
+  getter interval : Time::Span
+
+  def self.from_yaml(ctx : YAML::ParseContext, node : YAML::Nodes::Node) : BatchConfig
+    BatchConfig.new(ctx, node)
+  end
+end
+
+module YamlToAppender
+  def self.from_yaml(ctx : YAML::ParseContext, node : YAML::Nodes::Node)
+    appender_config = Hash(String, String).new(ctx, node)
+    case DiagnosticLogger::Config::AppenderClass.parse(appender_config["class"])
+    when DiagnosticLogger::Config::AppenderClass::FileAppender
+      path_to_file = appender_config["file"]
+      DiagnosticLogger::FileAppender.new(path_to_file, "a")
+    when DiagnosticLogger::Config::AppenderClass::ConsoleAppender
+      DiagnosticLogger::DefaultAppender
+    end.not_nil!
+  end
+end
+
+
 class DiagnosticLogger
   ConfigNamespace = "logger"
   DefaultLevel    = ::Logger::Severity::INFO
@@ -9,73 +44,21 @@ class DiagnosticLogger
   DefaultPattern  = "%{date} | [%{level}] %{pid}>%{fiber}>%{logger} | %{msg}"
 
   record Config,
-    level : ::Logger::Severity,
-    batch_config : BatchConfig?,
-    appender : IO,
-    pattern : String do
+    level : ::Logger::Severity = DiagnosticLogger::DefaultLevel,
+    batch_config : BatchConfig? = nil,
+    appender : IO = DiagnosticLogger::DefaultAppender,
+    pattern : String = DiagnosticLogger::DefaultPattern do
     enum AppenderClass
       ConsoleAppender
       FileAppender
     end
+    include YAML::Serializable
 
-    private module YamlToTimeSpan
-      def self.from_yaml(ctx : YAML::ParseContext, node : YAML::Nodes::Node) : Time::Span
-        unless node.is_a?(YAML::Nodes::Scalar)
-          node.raise "Expected scalar, not #{node.class}"
-        end
+    @[YAML::Field(key: "batch", converter: BatchConfig)]
+    getter batch_config : BatchConfig?
 
-        (node.value.to_f? || node.value.to_i).seconds
-      end
-    end
-
-    private abstract class YamlToAppender
-      def self.from_yaml(ctx : YAML::ParseContext, node : YAML::Nodes::Node)
-        appender_config = Hash(String, String).new(ctx, node)
-        case AppenderClass.parse(appender_config["class"])
-        when AppenderClass::FileAppender
-          path_to_file = appender_config["file"]
-          FileAppender.new(path_to_file, "a")
-        when AppenderClass::ConsoleAppender
-          DefaultAppender
-        end.not_nil!
-      end
-    end
-
-    protected record BatchConfig, size : Int32, interval : Time::Span do
-      YAML.mapping(
-        size: Int32,
-        interval: {
-          type:      Time::Span,
-          converter: YamlToTimeSpan,
-        }
-      )
-
-      def self.from_yaml(ctx : YAML::ParseContext, node : YAML::Nodes::Node) : BatchConfig
-        BatchConfig.new(ctx, node)
-      end
-    end
-
-    YAML.mapping(
-      level: {
-        type:    Logger::Severity,
-        default: DefaultLevel,
-      },
-      batch_config: {
-        key:       "batch",
-        type:      BatchConfig?,
-        default:   nil,
-        converter: BatchConfig,
-      },
-      appender: {
-        type:      IO,
-        default:   DefaultAppender,
-        converter: YamlToAppender,
-      },
-      pattern: {
-        type:    String,
-        default: DefaultPattern,
-      }
-    )
+    @[YAML::Field(converter: YamlToAppender)]
+    getter appender : IO
 
     def self.load(config : String | IO) : Config
       yml = YAML.parse config
